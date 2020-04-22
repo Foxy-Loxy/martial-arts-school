bb4e157f38b7525e02e3ad13c463e94272d84973
Kyrylo Avramenko <kavramenko2k@gmail.com>
2020-04-22 20:42:03 +0300

    added all necessary fields and validations for trainer model

--- src/models/Trainer.ts
+++ src/models/Trainer.ts
@@ -1,4 +1,6 @@
-import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+import { IsInt, IsDate, Min, Max, IsBoolean, IsEnum, IsString } from 'class-validator';
+import LevelType from '../constants/trainer';
 
 @Entity()
 export default class Trainer {
@@ -6,8 +8,71 @@ export default class Trainer {
     id!: number;
 
     @Column()
+    @IsString()
+    @Max(255)
     name!: string;
 
-    @CreateDateColumn()
-    createdAt!: string;
+    @Column()
+    @IsString()
+    @Max(255)
+    surname!: string;
+
+    @Column()
+    @IsString()
+    @Max(255)
+    fathersName!: string;
+
+    @Column()
+    @IsString()
+    @Max(255)
+    address!: string;
+
+    @Column()
+    @IsInt()
+    @Min(1)
+    @Max(10)
+    level!: number;
+
+    @Column({
+        type: 'enum',
+        enum: LevelType,
+    })
+    @IsEnum(LevelType)
+    levelType!: LevelType;
+
+    @Column()
+    @IsDate()
+    birthDate!: Date;
+
+    @Column()
+    @IsString()
+    @Max(255)
+    photo!: string;
+
+    @Column()
+    @IsString()
+    @Max(255)
+    title!: string;
+
+    @Column({
+        type: 'text',
+        length: 1024,
+    })
+    about!: string;
+
+    @Column({
+        update: false,
+    })
+    @IsBoolean()
+    readonly isMaster!: boolean;
+
+    @CreateDateColumn({
+        update: false,
+    })
+    readonly createdAt!: Date;
+
+    @UpdateDateColumn({
+        update: false,
+    })
+    readonly updatedAt!: Date;
 }
