24ed30ee2d012049102ac7cbb11ecb05dd9cbe89
Kyrylo Avramenko <kavramenko2k@gmail.com>
2020-04-25 22:03:32 +0300

    Updated configuration of db to use mysql. Moved to mysql2 driver

--- src/db.ts
+++ src/db.ts
@@ -1,15 +1,18 @@
-import { createConnection } from 'typeorm';
+import { Connection, createConnection } from 'typeorm';
 import Trainer from './models/Trainer';
 
-export default async function getDbConnection() {
+export default async function getDbConnection(): Promise<Connection> {
     const entities = [Trainer];
 
-    const conn = await createConnection({
-        type: 'sqlite',
-        database: ':memory:',
+    return createConnection({
+        type: 'mysql',
+        database: 'di_test',
+        username: 'root',
+        password: 'root',
+        host: 'localhost',
+        port: 3306,
         entities,
         synchronize: true,
+        insecureAuth: true,
     });
-
-    return conn;
 }
