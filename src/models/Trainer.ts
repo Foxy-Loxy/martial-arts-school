ce8c6b73f11af0ba8af7783262430f94a6952719
Kyrylo Avramenko <kavramenko2k@gmail.com>
2020-04-25 22:33:28 +0300

    Added newPassword property to enable password updating

--- src/models/Trainer.ts
+++ src/models/Trainer.ts
@@ -1,4 +1,12 @@
-import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn, BeforeInsert } from 'typeorm';
+import {
+    Column,
+    CreateDateColumn,
+    Entity,
+    PrimaryGeneratedColumn,
+    UpdateDateColumn,
+    BeforeInsert,
+    BeforeUpdate,
+} from 'typeorm';
 import { Exclude, classToPlain } from 'class-transformer';
 import bcrypt from 'bcrypt';
 import LevelType from '../constants/trainer';
@@ -62,9 +70,18 @@ export default class Trainer {
     })
     readonly updatedAt!: Date;
 
+    newPassword: string | undefined;
+
     @BeforeInsert()
     async hashPassword(): Promise<void> {
         this.password = await bcrypt.hash(this.password, 10);
+    }
+
+    @BeforeUpdate()
+    async hashNewPassword(): Promise<void> {
+        if (this.newPassword) {
+            this.password = await bcrypt.hash(this.newPassword, 10);
+        }
     }
 
     toJSON(): object {
