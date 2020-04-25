34d056f508db8c8805b1a5500f3ab5407b8fa72e
Kyrylo Avramenko <kavramenko2k@gmail.com>
2020-04-25 23:12:21 +0300

    Registered school repository

--- src/repositories/trainerRepository.ts
+++ src/repositories/trainerRepository.ts
@@ -1,7 +1,7 @@
 import { getConnection, Repository } from 'typeorm';
 import Trainer from '../models/Trainer';
 
-export default function getRepository(): Repository<Trainer> {
+export default function getTrainerRepository(): Repository<Trainer> {
     const conn = getConnection();
     return conn.getRepository(Trainer);
 }
