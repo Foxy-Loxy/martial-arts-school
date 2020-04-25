b0c9e253f594857a671032fe2a36c493032b1e8e
Kyrylo Avramenko <kavramenko2k@gmail.com>
2020-04-25 22:06:11 +0300

    Added trainer model dto. Added validation for dto. Implemented CRU for trainer

--- src/controllers/trainerController.ts
+++ src/controllers/trainerController.ts
@@ -1,18 +1,44 @@
-import { BaseHttpController, controller, httpGet, interfaces } from 'inversify-express-utils';
+import {
+    BaseHttpController,
+    controller,
+    httpGet,
+    httpPost,
+    interfaces,
+    requestBody,
+    requestParam,
+} from 'inversify-express-utils';
 import { JsonResult } from 'inversify-express-utils/dts/results';
 import { inject } from 'inversify';
-import { Repository } from 'typeorm';
+import { Repository, DeepPartial } from 'typeorm';
+import { validateOrReject } from 'class-validator';
 import TYPE from '../constants/types';
 import Trainer from '../models/Trainer';
+import CreateTrainerDto from '../models/dto/Trainer/createTrainerDto';
 
-@controller('/')
+@controller('/api/trainer')
 export default class TrainerController extends BaseHttpController implements interfaces.Controller {
-    constructor(@inject(TYPE.TrainerRepository) private fruitRepository: Repository<Trainer>) {
+    constructor(@inject(TYPE.TrainerRepository) private trainerRepository: Repository<Trainer>) {
         super();
     }
 
-    @httpGet('')
+    @httpGet('/')
     private async index(): Promise<JsonResult> {
-        return this.json(await this.fruitRepository.find());
+        return this.json(await this.trainerRepository.find());
+    }
+
+    @httpPost('/')
+    private async store(@requestBody() newTrainer: DeepPartial<CreateTrainerDto>): Promise<JsonResult> {
+        await validateOrReject(new CreateTrainerDto(newTrainer));
+        const trainerEntity = this.trainerRepository.create(newTrainer);
+        return this.json(await this.trainerRepository.save(trainerEntity));
+    }
+
+    @httpGet('/:id')
+    private async show(@requestParam('id') id: number): Promise<JsonResult> {
+        return this.json(
+            await this.trainerRepository.findOneOrFail({
+                id,
+            }),
+        );
     }
 }
